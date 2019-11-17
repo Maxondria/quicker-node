d63704c1bfed37ee7f32f007966dbecb5349bf85
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-11-17 12:00:06 +0300

    slow JS tasks - analysis

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require("express");
 
 const app = express();
 
+function doWork(duration) {
+  const start = Date.now();
+
+  while (Date.now() - start < duration) {}
+}
+
 app.get("/", (req, res) => {
+  doWork(5000); //Server blocked for 5s, all other requests must wait...
   res.send("Hi there...");
 });
 
