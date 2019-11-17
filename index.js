73839519852b57377643b04b5aaea7fd1e1a6e29
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-11-17 19:50:07 +0300

    refactor to use real life example

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//Restrict all slaves to have utmost 1 threadpool
+process.env.UV_THREADPOOL_SIZE = 1;
+
 const cluster = require("cluster");
 
 //Is the file being excecuted in cluster mode?
@@ -18,18 +21,22 @@ if (cluster.isMaster) {
 } else {
   // I am a slave, my job is being a server and nothing else
   const express = require("express");
+  const crypto = require("crypto");
 
   const app = express();
 
-  function doWork(duration) {
-    const start = Date.now();
+  //   function doWork(duration) {
+  //     const start = Date.now();
 
-    while (Date.now() - start < duration) {}
-  }
+  //     while (Date.now() - start < duration) {}
+  //   }
 
   app.get("/", (req, res) => {
-    doWork(5000); //Server blocked for 5s, all other requests must wait...
-    res.send("Hi there...");
+    //doWork(5000); //Server blocked for 5s, all other requests must wait...
+
+    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+      res.send("Hi there...");
+    });
   });
 
   app.get("/fast", (req, res) => {
