7b27d9413a3714c200cbf98899d56660502728e8
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-11-17 12:25:00 +0300

    cluster the app

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const express = require("express");
+const cluster = require("cluster");
 
-const app = express();
+//Is the file being excecuted in cluster mode?
+if (cluster.isMaster) {
+  /**
+   * Cause index.js to be executed again but in Slave mode,
+  in other works, a slave is created, on any requests now, they will go the 'else' part...,
+  This isn't help at the moment, only one worker slave created anyway*/
+  cluster.fork();
+} else {
+  // I am a slave, my job is being a server and nothing else
+  const express = require("express");
 
-function doWork(duration) {
-  const start = Date.now();
+  const app = express();
 
-  while (Date.now() - start < duration) {}
+  function doWork(duration) {
+    const start = Date.now();
+
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get("/", (req, res) => {
+    doWork(5000); //Server blocked for 5s, all other requests must wait...
+    res.send("Hi there...");
+  });
+
+  app.listen(3000);
 }
-
-app.get("/", (req, res) => {
-  doWork(5000); //Server blocked for 5s, all other requests must wait...
-  res.send("Hi there...");
-});
-
-app.listen(3000);
